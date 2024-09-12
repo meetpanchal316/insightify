29b7dd6108dfa68e224b1eaa6c5de6a064d7c0be
meetpanchal316 <90771988+meetpanchal316@users.noreply.github.com>
2024-09-12 17:02:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,6 @@ class MyApp extends StatelessWidget {
   Future<bool> verifySessionToken(String sessionToken) async {
     // Implement your logic to verify the session token against your server-side data
     // ...
-    return true; // Replace with your actual verification logic
+    return tru; // Replace with your actual verification logic
   }
 }
